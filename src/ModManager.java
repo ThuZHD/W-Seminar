8f5bde6a4244bc527fa2ebea151fb5fac9fbca67
Bruno B. <107209731+ThuZHD@users.noreply.github.com>
2026-06-03 12:11:58 +0200

    fixed window not opening on multiple platforms

--- src/ModManager.java
+++ src/ModManager.java
@@ -10,35 +10,35 @@ import java.util.ArrayList;
 public class ModManager {
 
     public ModManager() {
-        String dir = System.getProperty("user.dir");
-
-        try {
-            String jsonContent = new String(Files.readAllBytes(Paths.get(dir+ "/src/resources/Döner/kebab.json")));
-            JSONObject object = new JSONObject(jsonContent);
-//            System.out.println(object.getJSONArray("ingredients"));
-
-            JSONArray test = object.getJSONArray("ingredients");
-//            System.out.println(test.getJSONObject(10));
-
-            int cycle = 0;
-            while (true) {
-                try {
+//        String dir = System.getProperty("user.dir");
+//
+//        try {
+//            String jsonContent = new String(Files.readAllBytes(Paths.get(dir+ "/src/resources/Döner/kebab.json")));
+//            JSONObject object = new JSONObject(jsonContent);
+////            System.out.println(object.getJSONArray("ingredients"));
+//
+//            JSONArray test = object.getJSONArray("ingredients");
+////            System.out.println(test.getJSONObject(10));
+//
+//            int cycle = 0;
+//            while (true) {
+//                try {
 //                    System.out.println(test.getJSONObject(cycle));
-                    cycle++;
-                } catch (JSONException e) {
+//                    cycle++;
+//                } catch (JSONException e) {
 //                    System.out.println("stoped json cycle");
-                    break;
-                }
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        System.out.println("current dir = " + dir);
+//                    break;
+//                }
+//            }
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//        System.out.println("current dir = " + dir);
     }
 
     public JSONArray testFoodObject() {
